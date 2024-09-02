b0cd77ab97d11143d1215fcdac52ad82311c3a43
unknown <gufranlazuardi@gmail.com>
2024-09-02 14:55:00 +0700

    first commit

---
+++ controllers/contactController.js
@@ -0,0 +1,47 @@
+//@desc Get all contact
+//@route GET /api/contacts
+//@access public
+
+const getContacts = (req, res) => {
+    res.status(200).json({ message: "Get all contacts" });
+};
+
+//@desc Get contact by id
+//@route GET /api/contacts/:id
+//@access public
+
+const getContactById = (req, res) => {
+    res.status(200).json({ message: `Get contacts for ${req.params.id}` });
+};
+
+//@desc Create new contact
+//@route POST /api/contacts
+//@access public
+
+const createContact = (req, res) => {
+    res.status(201).json({ message: "Create contact" });
+};
+
+//@desc Update contact
+//@route PUT /api/contacts/:id
+//@access public
+
+const updateContact = (req, res) => {
+    res.status(201).json({ message: `Update contact for ${req.params.id}` });
+};
+
+//@desc Delete contact
+//@route DELETE /api/contacts/:id
+//@access public
+
+const deleteContact = (req, res) => {
+    res.status(201).json({ message: `Delete contact for ${req.params.id}` });
+};
+
+module.exports = {
+    getContacts,
+    createContact,
+    getContactById,
+    updateContact,
+    deleteContact,
+};
---
+++ routes/contactRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getContacts,
+    createContact,
+    getContactById,
+    updateContact,
+    deleteContact,
+} = require("../controllers/contactController");
+
+router.route("/").get(getContacts).post(createContact);
+
+router
+    .route("/:id")
+    .get(getContactById)
+    .put(updateContact)
+    .delete(deleteContact);
+
+module.exports = router;
