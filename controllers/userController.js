b09c5c97573a681d83c5e39935a4c279122f50df
gufranlazuardi <gufranlazuardi@gmail.com>
2024-09-03 14:36:07 +0700

    feat: user routes - login, register, current

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+
+//@desc Register a user
+//@route POST /api/users/register
+//@access public
+
+const registerUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Register the user" });
+});
+
+//@desc Login a user
+//@route POST /api/users/login
+//@access public
+
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Login the user" });
+});
+
+//@desc current user info
+//@route POST /api/users/current
+//@access public
+
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Login the user" });
+});
+
+module.exports = { registerUser, loginUser, currentUser };
