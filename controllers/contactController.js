a0c34bf82b30a10ed6235de4fd421e778407f107
gufranlazuardi <gufranlazuardi@gmail.com>
2024-09-03 12:54:12 +0700

    feat: get all contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 //@desc Get all contact
 //@route GET /api/contacts
 //@access public
 
 const getContacts = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get all contacts" });
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
 });
 
 //@desc Get contact by id
